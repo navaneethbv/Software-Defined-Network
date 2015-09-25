bc2984cf693c3d2b57b2dab06e0aeb02a77e8bf3
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-09-24 19:49:35 -0500

    Update plot-rate.sh

--- Master/assignment-2/plot-rate.sh
+++ Master/assignment-2/plot-rate.sh
@@ -2,7 +2,7 @@
 
 # Creates a graph of the bandwidth from a bandwidth log
 
-# Usage: ./plot-rate.sh ./student_folder/bwm.txt
+# Usage: ./plot-rate.sh ./student_folder/bwm.txt 
 # To use this script pass the path of the bandwidth log as the first argument.
 
 python util/plot_rate.py --rx \
